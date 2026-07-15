90b65b38c2347ba3a3af9fa63b0a3f8a1ebdb6b8
Shayaan <shayaan.musthafa2005@gmail.com>
2026-07-15 17:00:14 -0600

    Defined the book constructor and adding new books to an array

---
+++ script.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary(title, author, pages, isRead) {
+    const book = new Book(title, author, pages, isRead);
+    myLibrary.push(book);
+}
