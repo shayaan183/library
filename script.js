44bcee18f33a72347f4d4ba13110d64e298a9ad4
Shayaan <shayaan.musthafa2005@gmail.com>
2026-07-18 15:34:44 -0600

    Added a button that can change the read status for each book

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ function Book(title, author, pages, isRead) {
     this.isRead = isRead;
 }
 
+Book.prototype.toggleReadStatus = function() {
+    this.isRead = !this.isRead;
+    displayBooks();
+}
+
 function addBookToLibrary(title, author, pages, isRead) {
     const book = new Book(title, author, pages, isRead);
     myLibrary.push(book);
@@ -49,18 +54,27 @@ function displayBooks() {
         const isRead = document.createElement("p");
         isRead.textContent = book.isRead ? "Read" : "Not yet read";
 
+        const toggleReadButton = document.createElement("button");
+        toggleReadButton.textContent = book.isRead ? "Mark as Unread" : "Mark as Read";
+        toggleReadButton.classList.add("toggle-read-button");
+
+        toggleReadButton.addEventListener("click", () => {
+            book.toggleReadStatus();
+        });
+
         const removeButton = document.createElement("button");
         removeButton.textContent = "Remove Book";
         removeButton.classList.add("remove-button");
 
         removeButton.addEventListener("click", () => {
             removeBookFromLibrary(book.id);
-        })
+        });
 
         bookCard.appendChild(title);
         bookCard.appendChild(author);
         bookCard.appendChild(pages);
         bookCard.appendChild(isRead);
+        bookCard.appendChild(toggleReadButton);
         bookCard.appendChild(removeButton);
 
         container.appendChild(bookCard);
